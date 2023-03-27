e06f447907b56832ad06ffbf9fee6e84fda6107a
Tina Yang <Tinayangers@gmail.com>
2023-03-27 17:00:27 -0400

    Added form,input and button to signup page

--- manga-fi/src/pages/SignUp.vue
+++ manga-fi/src/pages/SignUp.vue
@@ -1,18 +1,30 @@
 <template>
-    <div>SignUp</div>
+    <div>
+        <h1 class="signupTitle">Sign Up</h1>
+        <form @submit="handleSubmit">
+            <input placeholder="Enter Full Name" :value="newName" @input="handleChange('newName')" class="addName">
+            <input placeholder="Enter Email" :value="newEmail" @input="handleChange('newEmail')" class="addName">
+            <input placeholder="Enter Password" :value="newPassword" @input="handleChange('newPassword')" class="addName">
+            <input placeholder="Enter Image Direct Link" :value="newImage" @input="handleChange('newImage')"
+                class="addName">
+            <button type="submit" class="addNameBtn">Register</button>
+        </form>
+    </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 
 export default {
     name: 'SignUp',
     data: () => ({
+
     }),
     mounted() {
 
     },
     methods: {
-
     }
 }
 </script>
