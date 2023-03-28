c4946fe74a5f63049f0955a76f615c798682c679
Tina Yang <Tinayangers@gmail.com>
2023-03-28 15:32:41 -0400

    Added get mangas method

--- manga-fi/src/pages/HomePage.vue
+++ manga-fi/src/pages/HomePage.vue
@@ -1,18 +1,26 @@
 <template>
-    <div>Home Page</div>
+    <div>
+        <h1 class="mangaFiTitle">Manga-Fi</h1>
+        <h2 class="slogan">Manga For Every Mood</h2>
+    </div>
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
     name: 'HomePage',
     data: () => ({
+        mangas: []
     }),
     mounted() {
-
+        this.getMangas()
     },
     methods: {
-
+        async getMangas() {
+            const res = await axios.get(`http://127.0.0.1:5000/mangalist`)
+            console.log(res)
+            mangas = res.data
+        }
     }
 }
 </script>
