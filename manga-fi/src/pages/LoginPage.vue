c5d0cb72d9999d78e7b4a9dbada64f517636cf0d
Tina Yang <Tinayangers@gmail.com>
2023-03-28 10:12:20 -0400

    Added login template

--- manga-fi/src/pages/LoginPage.vue
+++ manga-fi/src/pages/LoginPage.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>LoginPage</div>
+    <div>
+        <h1 class="loginTitle">Login</h1>
+        <form @submit='handleSubmit'>
+            <input placeholder="Enter Email" v-model="email" class="addName">
+            <input placeholder="Enter Password" v-model="password" class="addName">
+            <button type="submit" class="loginBtn">Log In</button>
+        </form>
+    </div>
 </template>
 
 <script>
