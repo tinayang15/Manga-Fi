a8dc16135da04dc88053fe246ff2aef9a66988c3
Tina Yang <Tinayangers@gmail.com>
2023-03-28 10:34:31 -0400

    Handle submit added to method

--- manga-fi/src/pages/LoginPage.vue
+++ manga-fi/src/pages/LoginPage.vue
@@ -10,16 +10,29 @@
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
     name: 'LoginPage',
     data: () => ({
+        email: '',
+        password: ''
     }),
     mounted() {
-
     },
     methods: {
-
+        async handleSubmit(e) {
+            e.preventDefault()
+            try {
+                const users = await axios.get(`http://127.0.0.1:5000/users`)
+                const user = users.data.find(user => user.email === this.email && user.password == this.password)
+                if (!user) {
+                    alert('Invalid email or password')
+                    return
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
     }
 }
 </script>
