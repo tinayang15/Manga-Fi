5593e18798fe664ee6fc6b6b67c82e0db7c9a245
Tina Yang <Tinayangers@gmail.com>
2023-03-30 11:17:34 -0400

    Added manga detail

--- manga-fi/src/pages/MangaDetail.vue
+++ manga-fi/src/pages/MangaDetail.vue
@@ -1,18 +1,37 @@
 <template>
-    <div>MangaDetail</div>
-</template>
+    <div>
+        <img :src="manga.cover_url" alt="">
+        <h1>{{ manga.title }}</h1>
+        <p>Author: {{ manga.author_name }}</p>
+        <p>Created </p>
+        <p>Description: {{ manga.description }}</p>
+        <p>{{ manga.publication_demographic }}</p>
+        <p>Genres: {{ manga.tags }}</p>
+    </div>
+</div></template>
 
 <script>
+import axios from 'axios';
+
 
 export default {
     name: 'MangaDetail',
     data: () => ({
+        manga: '',
+        chapters: []
     }),
     mounted() {
-
+        this.getMangaDetail()
     },
     methods: {
-
+        async getMangaDetail() {
+            const response = await axios.get(`http://127.0.0.1:5000/manga/${this.$route.params.manga_id}`)
+            // const response = await axios.get(`http://localhost:5000/manga/${this.$route.params.manga_id}`)
+            this.manga = response.data.data
+            this.chapters = response.data.data.chapters.data
+            console.log(response)
+            console.log(this.chapters)
+        }
     }
 }
 </script>
