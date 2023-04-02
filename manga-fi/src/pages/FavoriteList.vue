7e188d7ce44d3a957a0cbbd5d0f4f25e1f05d192
Tina Yang <Tinayangers@gmail.com>
2023-04-01 23:42:30 -0400

    Added get map props and get manga to favorite's list

--- manga-fi/src/pages/FavoriteList.vue
+++ manga-fi/src/pages/FavoriteList.vue
@@ -1,18 +1,41 @@
 <template>
-    <div>FavoriteList</div>
+    <div>
+        <h1>Your Favorites List</h1>
+        <div class="favoritesContainer" v-for="favorite in favorites" :key="favorite.id">
+            <p>List: {{ favorite }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 
 export default {
     name: 'FavoriteList',
     data: () => ({
+        favorites: [],
+        manga: []
     }),
     mounted() {
-
+        this.getFavorites()
     },
     methods: {
-
+        async getFavorites() {
+            const response = await axios.get(`http://127.0.0.1:5000/user_manga_lists`)
+            console.log(response)
+            this.favorites = response.data.map(mangaId => mangaId.favorite_list)
+            const newList = this.favorites[0].map(item => item)
+            this.getManga(newList)
+            console.log(this.favorites)
+            console.log(newList)
+        },
+        async getManga(newList) {
+            const response = await axios.get(`http://127.0.0.1:5000/manga/${newList[0]}`)
+            // const response = await axios.get(`http://localhost:5000/manga/${this.$route.params.manga_id}`)
+            this.manga = response.data.data
+            console.log(response)
+        },
     }
 }
 </script>
