5e3131de9fd9851a1ce6bdbc7d316e4e96e8eff9
Tina Yang <Tinayangers@gmail.com>
2023-04-03 14:26:34 -0400

    Added back to Manga to chapter details page

--- manga-fi/src/pages/MangaChapter.vue
+++ manga-fi/src/pages/MangaChapter.vue
@@ -1,10 +1,15 @@
 <template>
-    <div>
-        <h1>Time to Read Otaku</h1>
-        <div class="chapterContainer" v-for="data in chapterData" :key="data">
-            <img :src="baseUrl + '/data/' + hash + '/' + data" alt="">
-
+    <div class="chapterContainer">
+        <h1 class="title">Time to Read Otaku</h1>
+        <router-link :to="'/manga/' + $route.params.manga_id" class="link">
+            <button class="backBtn">Back To Manga</button>
+        </router-link>
+        <div class="detailContainer" v-for="data in chapterData" :key="data">
+            <img class="image" :src="baseUrl + '/data/' + hash + '/' + data" alt="">
         </div>
+        <router-link :to="'/manga/' + $route.params.manga_id" class="link">
+            <button class="backBtn">Back To Manga</button>
+        </router-link>
     </div>
 </template>
 
@@ -31,9 +36,29 @@ export default {
             this.baseUrl = response.data.data.baseUrl
             this.hash = response.data.data.hash
             console.log(response)
-        }
+        },
     }
 }
 </script>
 
-<style></style>
+<style scoped>
+.image {
+    height: 900px;
+    width: 500px;
+    border: 2px solid #D7BBA8;
+    box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.1);
+}
+
+.backBtn {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    background-color: #d9f9ed;
+    border-radius: 10px;
+    cursor: pointer;
+    padding: 10px;
+    width: 130px;
+    /* margin: 5px; */
+    font-weight: 700;
+    font-size: 12px;
+}
+</style>
