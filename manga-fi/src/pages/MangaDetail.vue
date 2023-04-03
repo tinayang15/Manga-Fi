a12d320d85b0f0e01d98e67cc5e90cb02fd3358b
Tina Yang <Tinayangers@gmail.com>
2023-04-03 12:13:33 -0400

    Added css to mangadetail

--- manga-fi/src/pages/MangaDetail.vue
+++ manga-fi/src/pages/MangaDetail.vue
@@ -1,47 +1,53 @@
 <template>
-    <div>
-        <img :src="manga.cover_url" alt="">
-        <h1>{{ manga.title }}</h1>
-        <button @click="handleFavorite" v-if="isAuthenticated !== isAuth">Add to Favorites</button>
-        <p>Author: {{ manga.author_name }}</p>
-        <p>Created: {{ manga.year }} </p>
-        <p>Description: {{ manga.description }}</p>
-        <p>{{ manga.publication_demographic }}</p>
-        <p>Genres: {{ manga.tags }}</p>
-        <div>
-            <h3>Chapters</h3>
+    <div class="headerContainer">
+        <img :src="manga.cover_url" alt="" class="image">
+        <div class="titleFavorite">
+            <h1 class="mangaTitle">{{ manga.title }}</h1>
+            <div class="btn">
+                <button class="favoriteBtn" @click="handleFavorite" v-if="isAuthenticated !== isAuth">Add to
+                    Favorites</button>
+            </div>
+        </div>
+        <p class="mangaDetail">Author: {{ manga.author_name }}</p>
+        <p class="mangaDetail">Created: {{ manga.year }} </p>
+        <p class="mangaDetail">Description: {{ manga.description }}</p>
+        <p class="mangaDetail">{{ manga.publication_demographic }}</p>
+        <p class="mangaDetail">Genres: {{ manga.tags }}</p>
+        <div class="chaptersContainer">
+            <h3 class="chaptersList">Chapters</h3>
             <div v-for="chapter in chapters" :key="chapter.id">
-                <!-- <router-link :to=`"/manga/" + manga.id + "/chapter/" + chapter.id`> -->
-                <router-link :to="'/manga/' + manga.id + '/chapter/' + chapter.id">
-                    <p>Chapter: {{ chapter.chapter }}</p>
+                <router-link :to="'/manga/' + manga.id + '/chapter/' + chapter.id" class="link">
+                    <p class="chapters">Chapter: {{ chapter.chapter }}</p>
                 </router-link>
             </div>
         </div>
         <div class="commentsContainer">
-            <h1>Comments</h1>
-            <div v-if="isAuthenticated !== isAuth">
-                <button @click="showAddCommentForm = !showAddCommentForm">Add Comment</button>
+            <h1 class="commentsList">Comments</h1>
+            <div class='comments' v-if="isAuthenticated !== isAuth">
+                <button @click="showAddCommentForm = !showAddCommentForm" class="addCommentBtn">Add Comment</button>
                 <form v-if="showAddCommentForm" @submit="handleSubmit">
                     <textarea placeholder="Share your thoughts, fellow otaku!" :value="newContent"
-                        @change="handleChange($event, 'newContent')" class='newCommentInput' rows="4" cols="50"></textarea>
+                        @change="handleChange($event, 'newContent')" class='input' rows="4" cols="50"></textarea>
                     <button type='submit' class="addComment">Submit</button>
                 </form>
 
-                <div v-for="comment in comments" :key="comment.id">
-                    <p>{{ comment.content }}</p>
+                <div v-for="comment in comments" :key="comment.id" class="commentDisplay">
+                    <p class="comment">{{ comment.content }}</p>
 
-                    <button @click="handleDelete(comment)">Delete</button>
+                    <button class='deleteBtn' @click="handleDelete(comment)">Delete</button>
 
-                    <button @click="showingUpdateForm[comment.id] = !showingUpdateForm[comment.id]">Edit</button>
+                    <button class="updateBtn"
+                        @click="showingUpdateForm[comment.id] = !showingUpdateForm[comment.id]">Edit</button>
                     <form v-if="showingUpdateForm[comment.id]" @submit="handleUpdate($event, comment)">
-                        <textarea :value="comment.content" @change="handleChange($event, 'updateContent')"
-                            class='newContentInput' rows="4" cols="50"></textarea>
+                        <textarea class='input' :value="comment.content" @change="handleChange($event, 'updateContent')"
+                            rows="4" cols="50"></textarea>
                         <button type='submit' class="addComment">Submit</button>
                     </form>
                 </div>
             </div>
-            <div v-else>
-                <p>Please <router-link to='/login'>sign in</router-link> to add or edit/delete comments.</p>
+            <div v-else class="comments">
+                <p class="login">Please <router-link class='link' to='/login'>sign in</router-link> to add or edit/delete
+                    comments.</p>
             </div>
 
         </div>
@@ -50,8 +56,6 @@
 
 <script>
 import axios from 'axios';
-
-
 
 export default {
     name: 'MangaDetail',
@@ -76,13 +80,11 @@ export default {
         getAuthentication() {
             const auth = localStorage.getItem('isAuthenticated')
             this.isAuth = Boolean(auth)
-            // console.log(isAuthenticated)
             console.log(this.isAuth)
             console.log(auth)
         },
         async getMangaDetail() {
             const response = await axios.get(`http://127.0.0.1:5000/manga/${this.$route.params.manga_id}`)
-            // const response = await axios.get(`http://localhost:5000/manga/${this.$route.params.manga_id}`)
             this.manga = response.data.data
             this.chapters = response.data.data.chapters.data
             console.log(response)
@@ -113,14 +115,10 @@ export default {
             const response = await axios.delete(`http://127.0.0.1:5000/comments/${comment.id}`)
             console.log(response)
             this.getMangaComment()
-            // const index = this.comments.findIndex(c => c.id === comment.id);
-            // this.comments.splice(index, 1);
         },
         async handleUpdate(e, comment) {
-            // console.log('here', comment)
             e.preventDefault()
             const updatedComment = {
-                // ...comment,
                 content: this.updateContent
             }
             const res = await axios.put(`http://127.0.0.1:5000/comments/${comment.id}`, updatedComment)
@@ -128,20 +126,6 @@ export default {
             this.showingUpdateForm[comment.id] = false
             this.getMangaComment()
         },
-        // async handleFavorite() {
-        //     const favorite = {
-        //         user_id: localStorage.getItem('user_id'),
-        //         manga_id: `${this.$route.params.manga_id}`,
-        //         favorite_list: [`${this.$route.params.manga_id}`]
-        //     }
-        //     this.favorites.push(favorite)
-        //     console.log("new", favorite)
-        //     const userId = localStorage.getItem('user_id')
-        //     const userFavorites = await axios.get(`http://127.0.0.1:5000/user_manga_lists/user/${userId}`)
-        //     const response = await axios.post(`http://127.0.0.1:5000/user_manga_lists`, favorite)
-        //     const res = await axios.put(`http://127.0.0.1:5000/user_manga_lists`, favorite)
-        //     console.log("here", response)
-        // }
         async handleFavorite() {
             const userId = localStorage.getItem('user_id');
             const getFavoritesResponse = await axios.get(`http://127.0.0.1:5000/user_manga_lists/user/${userId}`);
@@ -150,19 +134,38 @@ export default {
                 const res = await axios.post(`http://127.0.0.1:5000//user_manga_lists`, { user_id: localStorage.getItem('user_id'), manga_id: `${this.$route.params.manga_id}`, favorite_list: [`${this.$route.params.manga_id}`] });
                 console.log(res)
             } else {
-                // const favoriteListId = getFavoritesResponse.data[0].id;
-                // const response = await axios.put(`http://127.0.0.1:5000/user_manga_lists/user/${userId}`, { favorite_list: [`${this.$route.params.manga_id}`] });
-                // console.log(response)
                 const favoriteListId = getFavoritesResponse.data[0].id;
                 const currentFavorites = getFavoritesResponse.data[0].favorite_list;
                 currentFavorites.push(`${this.$route.params.manga_id}`);
                 const response = await axios.put(`http://127.0.0.1:5000/user_manga_lists/${favoriteListId}`, { favorite_list: currentFavorites });
                 console.log(response)
             }
-            // this.getFavorites();
         }
     }
 }
 </script>
 
-<style></style>
+<style scoped>
+img {
+    width: 300px;
+    height: 400px;
+}
+
+.titleFavorite {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+
+.favoriteBtn {
+    margin-right: -20px;
+    margin-left: 140px;
+}
+
+.mangaTitle {
+    margin-left: 220px;
+    max-width: 400px;
+}
+</style>
